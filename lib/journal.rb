c0f7d7420cc1b82129f26146ba44994178835894
Harley Trung <harley@socialsci.com>
2012-06-16 19:09:34 -0400

    first real test

--- lib/journal.rb
+++ lib/journal.rb
@@ -1,3 +1,8 @@
 class Journal
+  attr_accessor :name, :url
 
+  def initialize(hash = {})
+    @name = hash[:name]
+    @url  = hash[:url]
+  end
 end
--- spec/lib/journal_spec.rb
+++ spec/lib/journal_spec.rb
@@ -1,7 +1,11 @@
 require File.expand_path('spec/spec_helper')
 
 describe Journal do
-  it "should work" do
-    1.should == 1
+  context "initialization" do
+    it "should set attributes" do
+      journal = Journal.new name: "google", url: "http://www.google.com"
+      journal.name.should == "google"
+      journal.url.should  == "http://www.google.com"
+    end
   end
 end
