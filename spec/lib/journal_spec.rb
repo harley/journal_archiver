99c17d42c66e2c3b0fbbc8fc1d0ab85d3af38fb2
Harley Trung <harley@socialsci.com>
2012-06-16 19:05:19 -0400

    setup rspec

---
+++ spec/lib/journal_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path('spec/spec_helper')
+
+describe Journal do
+  it "should work" do
+    1.should == 1
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require File.expand_path("journal_archiver.rb")
